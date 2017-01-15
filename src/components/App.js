14e2d49fc4a0a5e0f736c1573cd8ba7d129b1a5b
Alisher778 <internet-docs@mail.ru>
2017-01-15 11:20:18 -0500

    cleaned up and organized folder structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default App;
